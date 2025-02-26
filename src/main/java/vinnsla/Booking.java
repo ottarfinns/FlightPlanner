ff280414c9fa22c38709efa7165ceb639323ec42
ottarfinns <ottarfinns@gmail.com>
2025-02-25 20:28:59 -0500

    First drafts for Booking and Customer attributes

--- src/main/java/vinnsla/Booking.java
+++ src/main/java/vinnsla/Booking.java
@@ -1,6 +1,19 @@
 package vinnsla;
 public class Booking {
+
+    private int bookingId;
+
+    private Flight flight;
+    private Customer customer;
+
+    // Need format for seat
+    private String seat;
+
+    private boolean carryon;
+    private boolean firstClass;
+
+
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/main/java/vinnsla/Customer.java
@@ -0,0 +1,16 @@
+package vinnsla;
+
+public class Customer {
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String passportNumber;
+    // More info on customer
+
+
+    public static void main(String[] args) {
+
+    }
+}
