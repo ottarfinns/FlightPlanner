8c6586e044865e566e7a64b612c83e5bfc72dea8
ottarfinns <ottarfinns@gmail.com>
2025-03-31 14:18:25 +0000

    Búa til klasa fyrir booking

--- src/main/java/vinnsla/entities/Booking.java
+++ src/main/java/vinnsla/entities/Booking.java
@@ -11,8 +11,45 @@ public class Booking {
     private boolean firstClass;
 
     // Getters and setters
+    public int getBookingId() {
+        return bookingId;
+    }
 
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
 
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public boolean isCarryon() {
+        return carryon;
+    }
+
+    public void setCarryon(boolean carryon) {
+        this.carryon = carryon;
+    }
+
+    public boolean isFirstClass() {
+        return firstClass;
+    }
+
+    public void setFirstClass(boolean firstClass) {
+        this.firstClass = firstClass;
+    }
 
     public static void main(String[] args) {
 
---
+++ src/main/java/vinnsla/repository/BookingRepoMock.java
@@ -0,0 +1,8 @@
+package vinnsla.repository;
+
+public class BookingRepoMock implements BookingRepositoryInterface {
+
+    public static void main(String[] args) {
+
+    }
+}
