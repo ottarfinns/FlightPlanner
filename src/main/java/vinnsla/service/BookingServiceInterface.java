1a186c43b8b75950f66e0be6ece70319246a5309
ottarfinns <ottarfinns@gmail.com>
2025-04-02 11:14:18 +0000

    Booking controller, service og interface bætt við

--- src/main/java/vinnsla/service/BookingServiceInterface.java
+++ src/main/java/vinnsla/service/BookingServiceInterface.java
@@ -1,5 +1,14 @@
 package vinnsla.service;
 
-public interface BookingServiceInterface {
+import vinnsla.entities.Booking;
+import java.util.List;
 
+public interface BookingServiceInterface {
+    boolean createBooking(Booking booking);
+    List<Booking> getAllBookings();
+    Booking getBooking(int bookingId);
+    boolean updateBooking(Booking booking);
+    boolean deleteBooking(int bookingId);
+    boolean processPayment(int bookingId, double amount);
+    double calculateTotalPrice(Booking booking);
 }
