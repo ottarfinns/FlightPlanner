ed0fbe84cfa1113eb97751b019b50256f5aebc7f
ottarfinns <ottarfinns@gmail.com>
2025-04-02 18:08:46 +0000

    Bæta við viðmóti

--- src/main/java/vidmot/flightplanner/FlightApplication.java
+++ src/main/java/vidmot/flightplanner/FlightApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class FlightApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FlightApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FlightApplication.class.getResource("Booking-View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
